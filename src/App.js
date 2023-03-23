a71acaf4f02e7272eb9213da72f4aa98202f8a27
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-03-23 20:15:49 +0530

    redirecting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Products from "./pages/Products";
 import CartProvider from "./components/store/CartProvider";
 import AboutUs from "./pages/AboutUs";
@@ -8,21 +8,28 @@ import ContactUs from "./pages/ContactUs";
 import ProductDetail from "./pages/ProductDetail";
 import Login from "./pages/Login";
 import RootLayout from "./pages/Root";
+import AuthContext from "./components/store/auth-context";
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <CartProvider>
       <RootLayout>
         <Switch>
-          <Route path="/" exact>
-            <Products />
-          </Route>
+          
+            <Route path="/" exact>
+            {authCtx.isLoggedIn && (<Products />)}
+            {!authCtx.isLoggedIn && <Redirect to="/login" />}
+            </Route>
+          
+          
+
           <Route path="/about">
             <AboutUs />
           </Route>
           <Route path="/home">
             <HomePage></HomePage>
           </Route>
-          
+
           <Route path="/login">
             <Login></Login>
           </Route>
